37bd0a466f8ee891c44f732af45025ffdb5c33ce
Bruno Kanazawa <kanazawabruno@gmail.com>
2018-12-19 15:36:46 -0200

    Fix issue on create DB

--- deezer_dl/base.py
+++ deezer_dl/base.py
@@ -1,11 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
-
-eng_path =  os.path.join(os.path.expanduser('~'), 'deezer_dl', 'deezerdl.db')
-
+_path = os.path.join(os.path.expanduser('~'), 'deezer_dl')
+os.makedirs(_path, exist_ok=True)
+eng_path = os.path.join(_path, 'deezerdl.db')
 
 engine = create_engine('sqlite:///{eng_path}'.format(eng_path=eng_path))
 Session = sessionmaker(bind=engine)
